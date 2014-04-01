fd28c7065bb794f64c8bd0921f59a9efbcdbd4e2
Becky Bell <rlbell@alum.bu.edu>
2014-04-01 13:07:46 -0400

    created new repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Gitpractice::Application.config.secret_key_base = '78aeb0fd8270b23657fe6094bbf3c2d2f744d139fc2d41f4bad1c6e9e5d6fdeeeec92e0a096da9d405beb8ec5ce4185e3f27e40d4c3e61458e2af493cee5d7a6'
